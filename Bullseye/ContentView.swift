9795603440c99d737e7897b4c1b4cd37d941d3ac
Maria Boseke <mboseke@gmail.com>
2020-06-09 20:54:40 -0400

    initial commit with a simple UI for the bullseye game

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Maria Boseke on 5/27/20.
+//  Copyright © 2020 Maria Boseke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    @State var target: Int = Int.random(in: 1...100)
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Spacer()
+                
+                //Target row
+                HStack {
+                    Text("Put the bullseye as close as you can to:")
+                    Text("\(self.target)")
+                }
+                Spacer()
+                
+                //Slider row
+                HStack {
+                    Text("1")
+                    Slider(value: self.$sliderValue, in: 1...100)
+                    Text("100")
+                }
+                Spacer()
+                
+                //Button row
+                Button(action: {
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) { () -> Alert in
+                    let roundedValue: Int = Int(self.sliderValue.rounded())
+                    return Alert(title: Text("Hello there"),
+                                 message: Text("The slider's value is: \(roundedValue). \n " +
+                                    "You scored \(self.pointsForCurrentRound()) points this round."),
+                                 dismissButton: .default(Text("Awesome")))
+                }
+                Spacer()
+                
+                //Score row
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Start Over")
+                    }
+                    Spacer()
+                    Text("Score")
+                    Text("999999")
+                    Spacer()
+                    Text("Round")
+                    Text("999")
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Info")
+                    }
+                }.padding(.bottom, 20)
+                
+            }
+        }
+    }
+    
+    func pointsForCurrentRound() -> Int {
+        let roundedValue: Int = Int(self.sliderValue.rounded())
+        var difference: Int = self.target - roundedValue
+        
+        if difference < 0 {
+            difference *= -1
+        }
+
+        let awardedPoints: Int = 100 - difference
+        
+        return awardedPoints
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
+    }
+}
